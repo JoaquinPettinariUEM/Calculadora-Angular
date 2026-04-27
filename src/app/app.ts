21cfb13fe9a11c60d697b66a70684016654d1e12
joaquin.pettinari <joaquin.pettinari@hotmail.com>
2026-04-27 22:01:12 +0200

    Calculator solved

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('calculadora');
+  display = '';
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+  onAdd() {
+    if (this.display.at(-1) === '+') {
+      return;
+    }
+    if (!this.display.length) {
+      this.display = '0+';
+      return;
+    }
+    this.display += '+';
+  }
+
+  onTotal() {
+    const numbersSplitted = this.display.split('+').map(Number);
+    const total = numbersSplitted.reduce((partialSum, acc) => partialSum + acc, 0);
+    this.display = String(total);
+  }
+
+  onNumberClick(num: number) {
+    this.display += num;
+  }
+
+  onRemoveLastOne() {
+    if (this.display.length) {
+      this.display = this.display.slice(0, -1);
+    }
+  }
+
+  onRestart() {
+    this.display = '';
+  }
 }
